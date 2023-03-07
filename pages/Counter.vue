3b8b33d1bafccd78ce0e6b559ba6eb95204ad091
James <lanzonprojects@gmail.com>
2023-03-07 02:00:40 +0100

    Scaffolding including new test Counter page
    By using ChatGBT, I am creating a test web-app which consists of NuxtJS using Pinia for VueJS that is compatible with SSR which will be hosted on Netlify.

---
+++ pages/Counter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button @click="increment">
+      Increment
+    </button>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import { useStore } from 'pinia'
+
+export default defineComponent({
+  setup () {
+    const store = useStore()
+
+    const count = store.state.count
+
+    const increment = () => {
+      store.commit('increment')
+    }
+
+    return { count, increment }
+  }
+})
+</script>
